39ac7b18b4ff8435e14bc62c1e9b3d8a2c6fb002
Jayden Back <gmouse03@gmail.com>
2018-01-28 09:32:10 +1300

    edit spelling error - require

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = requrie('webpack');
+const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
